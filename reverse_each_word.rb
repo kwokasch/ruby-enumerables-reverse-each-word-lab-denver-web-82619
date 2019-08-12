0cb06f6079f1c25184d540f2f29a519fe56f33c5
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-12 21:28:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,4 +7,11 @@ def reverse_each_word(string)
   new_array.join(" ")
 end 
 
-def 
+def reverse_each_word(string)
+  current_array = string.split(" ")
+  new_array = []
+  current_array.collect do |string|
+    new_array << string.reverse 
+  end
+  new_array.join(" ")
+end  
